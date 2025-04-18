a6b579f835d6a23dc50d56eb801b9857f2574bc3
HuanHuanHuanFFF <2445657276@qq.com>
2025-04-18 20:42:50 +0800

    封装性能会有损失

--- MAT/2024HeiBeiCPC-J.cpp
+++ MAT/2024HeiBeiCPC-J.cpp
@@ -6,16 +6,10 @@ constexpr int MOD = 1e9 + 7;
 
 struct MAT {
     int n, m;
-    vector<vector<ll> > ele;
+    ll ele[3][3]{};
 
     MAT() {
-    }
-
-    MAT(int n, int m): n(n), m(m) {
-        ele = vector(n + 1, vector<ll>(m + 1));
-    }
-
-    MAT(const MAT &other): n(other.n), m(other.m), ele(other.ele) {
+        n = 3, m = 3;
     }
 
     //重载运算符,方便使用
@@ -23,27 +17,8 @@ struct MAT {
         return ele[x][y];
     }
 
-    //打印矩阵
-    void print() {
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j)
-                cout << ele[i][j] << " ";
-            cout << "\n";
-        }
-    }
-
-    //返回大小为n*n的单位矩阵
-    static MAT identity(int n) {
-        MAT res(n, n);
-        for (int i = 1; i <= n; ++i) {
-            res(i, i) = 1;
-        }
-        return res;
-    }
-
     MAT operator *(const MAT &b) const {
-        assert(m == b.n);
-        MAT ans(n, b.m);
+        MAT ans;
         for (int i = 1; i <= n; ++i)
             for (int j = 1; j <= b.m; ++j)
                 for (int k = 1; k <= m; ++k)
@@ -75,7 +50,7 @@ void AC() {
     int m;
     cin >> m;
     vector<int> cnt(10);
-    MAT ans(1, 2), base(2, 2);
+    MAT ans, base;
     for (auto &x: cnt) cin >> x;
     if (m == 1 && cnt[0] > 0) {
         cout << "0\n";
