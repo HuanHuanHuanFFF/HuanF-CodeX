16ca8dca075db48d9ca01c788d2365fccf91d9eb
HuanHuanHuanFFF <2445657276@qq.com>
2025-07-28 16:08:05 +0800

    通用模板

---
+++ temp.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::pair;
+using std::vector;
+using std::string;
+
+using ll = long long;
+using ull = unsigned long long;
+using PII = pair<int, int>;
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9 + 17;
+constexpr int N = 1e7 + 7;
+
+std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+
+void IOS() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+auto get_now() {
+    return std::chrono::high_resolution_clock::now();
+}
+
+auto ms_dis(auto t2, auto t1) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+}
+
+void init() {
+    IOS();
+}
+
+constexpr bool more = true;
+
+void HuanF() {
+}
+
+signed main() {
+    init();
+    int T = 1;
+    if (more) cin >> T;
+    while (T-- > 0) HuanF();
+    return 0;
+}
