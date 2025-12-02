fffe143a39ad17c5fc8621073826ea36d9d56e93
HuanHuanHuanFFF <2445657276@qq.com>
2025-12-02 18:31:27 +0800

    cf 1065 div3

---
+++ Code Instrumentality Initiative/CF/CF 1065 div3/D_Rae_Taylor_and_Trees_easy_version.cpp
@@ -0,0 +1,67 @@
+//
+// Created by 幻 on 2025/11/30.
+//
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::pair;
+using std::vector;
+using std::string;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using PII = pair<int, int>;
+constexpr ll MOD = 1e9 + 7;
+constexpr ll INF = 1e9 + 17;
+constexpr ll N = 1e7 + 7;
+
+std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+
+void IOS() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+auto get_now() {
+    return std::chrono::high_resolution_clock::now();
+}
+
+auto ms_dis(auto t2, auto t1) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+}
+
+void init() {
+    IOS();
+}
+
+constexpr bool more = true;
+
+// Codeforces Round 1065 (Div. 3) 
+// D. Rae Taylor and Trees (easy version)
+void HuanF() {
+    int n;
+    cin >> n;
+    vector<int> p(n + 1), pre(n + 1, INF), suf(n + 2);
+    for (auto &x: p | std::views::drop(1)) cin >> x;
+    for (int i = 1; i <= n; ++i) pre[i] = std::min(pre[i - 1], p[i]);
+    for (int i = n; i >= 1; --i) suf[i] = std::max(suf[i + 1], p[i]);
+    for (int i = 2; i <= n; ++i) {
+        if (pre[i - 1] > suf[i]) {
+            cout << "No\n";
+            return;
+        }
+    }
+    cout << "Yes\n";
+}
+
+signed main() {
+    init();
+    int T = 1;
+    if (more) cin >> T;
+    while (T-- > 0) HuanF();
+    return 0;
+}
