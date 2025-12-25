c6ff1d34da9796ff36a8d7685712e772a7f40b13
HuanHuanHuanFFF <2445657276@qq.com>
2025-12-25 17:57:12 +0800

    LC水题

---
+++ Code Instrumentality Initiative/LC/DP/63.go
@@ -0,0 +1,31 @@
+package DP
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	n, m := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, m)
+	}
+	for i := 0; i < m; i++ {
+		if obstacleGrid[0][i] != 1 {
+			dp[0][i] = 1
+		} else {
+			break
+		}
+	}
+	for i := 0; i < n; i++ {
+		if obstacleGrid[i][0] != 1 {
+			dp[i][0] = 1
+		} else {
+			break
+		}
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if obstacleGrid[i][j] != 1 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[n-1][m-1]
+}
---
+++ Code Instrumentality Initiative/LC/daily/3075.go
@@ -0,0 +1,13 @@
+package daily
+
+import "slices"
+
+func maximumHappinessSum(happiness []int, k int) int64 {
+	slices.Sort(happiness)
+	slices.Reverse(happiness)
+	ans := int64(0)
+	for i := 0; i < k; i++ {
+		ans += int64(max(happiness[i]-i, 0))
+	}
+	return ans
+}
