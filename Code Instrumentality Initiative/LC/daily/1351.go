483d2312ee8e3ad5cf9519367b093e5a9d1e6e0c
HuanHuanHuanFFF <2445657276@qq.com>
2025-12-28 23:28:10 +0800

    leetcode daily

---
+++ Code Instrumentality Initiative/LC/daily/1351.go
@@ -0,0 +1,16 @@
+package daily
+
+func countNegatives(grid [][]int) int {
+	ans, n, m := 0, len(grid), len(grid[0])
+	x, y := 0, m
+	back := 0
+	for x < n && y >= 0 {
+		for y > 0 && grid[x][y-1] < 0 {
+			y--
+			back++
+		}
+		ans += back
+		x++
+	}
+	return ans
+}
